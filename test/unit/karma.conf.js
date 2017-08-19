e33b6f7a9f0534dba14282b5aa237d94e4138262
caiqiang_shi <caiqiang_shi@kingdee.com>
2017-08-19 10:26:01 +0800

    fix

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -7,13 +7,10 @@ const webpackConfig = require('../../build/webpack.test.conf');
 
 module.exports = function (config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
-    reporters: ['spec', 'coverage', 'progress'],
+    reporters: ['spec', 'progress'],
+    // 'coverage',
     files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap'],
@@ -29,5 +26,7 @@ module.exports = function (config) {
         { type: 'text-summary' },
       ],
     },
+    autoWatch: true,
+    concurrency: Infinity,
   });
 };
